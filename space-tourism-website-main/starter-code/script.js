cbbb48a3764b2d14a325b7f2bad3d54fea1a59df
hadjer-b1 <hadjerbelahbib3@gmail.com>
2025-05-22 21:25:06 +0100

    Added crew page js

--- space-tourism-website-main/starter-code/script.js
+++ space-tourism-website-main/starter-code/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   menuIcon.addEventListener("click", () => {
     nav.classList.toggle("show");
   });
+
   const navLinks = document.querySelectorAll(".Navbar li a");
   navLinks.forEach((link) => {
     if (link.href.includes(window.location.pathname)) {
@@ -11,15 +12,61 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // here if i use an icon in mobile mode:
+  //---------------- here if i use an icon in mobile mode:------------------------------------
   const moonIcon = document.querySelector(".star");
   const miniNav = document.querySelector(".mini-nav");
-  moonIcon.addEventListener("click", () => {
+  /*moonIcon.addEventListener('click', () => {
     miniNav.classList.toggle("show");
-  });
+  });*/
+  /*-----------------------------Crew Page--------------------------- */
+const dots = document.querySelectorAll(".dot");
+const crewPosition = document.querySelector(".position")
+const crewName = document.querySelector(".name");
+const crewDetails = document.querySelector(".details");
+const crewImage = document.querySelector("crew-img");
+ 
+const CrewMembers = [{
+            Position :"Commander",
+            Name : "Douglas Hurley",
+            Details :" Douglas Gerald Hurley is an American engineer, former Marine Corps pilot  and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+            imgSrc :"assets/crew/image-douglas-hurley.webp"
+          },
+          {
+            Position:"Pilot",
+            Name: "Victor Glover",
+            Details:"Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ",
+            imgSrc:"assets/crew/image-victor-glover.webp"
+          },
+          {
+            Position:"Mission Specialist",
+            Name: "Mark Shuttleworth",
+            Details:"Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems.  Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+            imgSrc:"assets/crew/image-mark-shuttleworth.webp"
+          },
+          {
+            Position:"Flight Engineer",
+            Name: "Anousheh Ansari",
+            Details:"Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind  the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist. ",
+            imgSrc:"assets/crew/image-anousheh-ansari.webp"
+          }
+]
 
-  // -----------------------------Distenation:
-  const destinationContainer = document.querySelector(".destination-container");
+/*dots.forEach((dot) => {
+    dot.addEventListener('click', () =>{
+      /*const ID = dot.getattribute("id");
+      const member = CrewMembers[`${ID}`];
+
+      crewPosition.textContent = member.Position ;
+      crewName.textContent = member.Name ;
+      crewDetails.textContent = member.Details;
+      crewImage.src = member.imgSrc;    
+      dots.forEach(oldDot => oldDot.remove('switchActive'));
+       dot.classList.add('switchActive');
+       dot.style.width= "300px";
+    });
+    
+    });*/
+  // -----------------------------Distenation Page:----------------------------------------------
   const moons = document.querySelectorAll(".mini-nav ul li");
 
   /*function fetchingEvent(moon) {
@@ -95,10 +142,12 @@ document.addEventListener("DOMContentLoaded", () => {
         break;
     }
   }*/
-  // Add event listeners to each moon
+  
+    // Add event listeners to each moon
   moons.forEach((moon) => {
     moon.addEventListener("click", () => {
        window.location = `destination-${moon.id}.html`;
     });
   });
-});
+  });
+
